0b82bcfc6bc585b979a073914e65ddc3f846083d
alwinv135 <alwin_villamor@mail.fresnostate.edu>
2019-10-26 23:00:01 -0700

    Login page Change

--- AwesomeProject/src/screens/LoginScreen.js
+++ AwesomeProject/src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
 import useGlobalState from '../globalState'
 
 import auth from '@react-native-firebase/auth';
@@ -46,19 +46,41 @@ export default LoginScreen = ({navigation}) => {
             navigation.navigate("App")
     },[loggedIn])
 
-  return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text style={{width: "100%", textAlign: "center", color: "red"}}>{err}</Text>
-        <TextInput value={email} onChangeText={(e)=>setEmail(e)}
-            style={{backgroundColor: "grey", borderWidth: 2, borderColor: "black", width: "80%"}}/>
-        <TextInput value={pass} secureTextEntry={true} onChangeText={(e)=>setPass(e)}
-            style={{backgroundColor: "grey", borderWidth: 2, borderColor: "black", width: "80%"}}/>
-        <TouchableOpacity onPress={handlePress} style={{height: 100, width: "80%", justifyContent: "center", alignItems: "center", backgroundColor: "grey"}}>
-            <Text>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=>navigation.navigate("Signup")} style={{justifyContent: "center", alignItems: "center", marginTop: 10}}>
-            <Text>No account?</Text>
-        </TouchableOpacity>
-    </View>
-  );
+    return(
+        <View style= {styles.container}>
+            <Text style={styles.welcome}>GPSRPG</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+            />
+            <View style={styles.btnContainer}>
+                <TouchableOpacity
+                    onPress={handlePress}
+                    style={styles.userBtn}
+                >
+                    <Text style={styles.btnTxt}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={()=>navigation.navigate("Signup")}
+                    style={styles.userBtn}
+                >
+                    <Text style={styles.btnTxt}>Signup</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+	container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#05ffb0"},
+	welcome: { fontSize: 30, textAlign: 'center', margin: 10, color: "red", fontFamily: "DancingScript-Bold"},
+	input:{ width: '90%', backgroundColor: "#fff", padding: 15, marginBottom: 10},
+	btnContainer:{ flexDirection: "row", justifyContent: "space-between", width: "90%"},
+	userBtn:{ backgroundColor: "#ffd700", padding: 15, width: "45%"},
+	btnTxt:{ fontSize: 18, textAlign: 'center'}
+});
