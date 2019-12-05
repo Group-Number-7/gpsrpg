89e7c29049a52a9c1904780a6e24beefe2f0552a
dr <dustinnrutherford@gmail.com>
2019-12-05 13:25:38 -0800

    Scroll View (flatlist) Working
    UI alignment could still use a lot of work, but a scroll view with some clickable abiliy icons is currently working.

--- AwesomeProject/src/assets/spell_info.js
+++ AwesomeProject/src/assets/spell_info.js
@@ -1,5 +1,6 @@
 export default SpellInfo = {
-    frostarmor: {title: "Frost Armor", decription: "Increases armor. Chills melee attackers, slowing their attack speed."},
-    healingtouch: {title: "Healing Touch", decription: "Heals target for N HP."},
-    shiv: {title: "Shiv", decription: "Attacks target with melee weapon, doing 50% of weapon damage. Low chance of being blocked."}
+    frostarmor: {title: "Frost Armor", description: "Increases armor. Chills melee attackers, slowing their attack speed."},
+    healingtouch: {title: "Healing Touch", description: "Heals target for N HP."},
+    shiv: {title: "Shiv", description: "Attacks target with melee weapon, doing 50% of weapon damage. Low chance of being blocked."},
+    snare: {title: "Snare", description: "Lowers target's speed."}
 }
--- AwesomeProject/src/screens/AbilitiesScreen.js
+++ AwesomeProject/src/screens/AbilitiesScreen.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, FlatList } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, FlatList, Dimensions, Button } from 'react-native'
 import Axios from 'axios'
 import SpellIcons from '../assets/images/spell_icons'
 import SpellInfo from '../assets/spell_info'
 import useGlobalState from '../globalState'
 import constants from '../config/constants'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default AbilitiesScreen = ({navigation}) => {
     const [selectedType, setSelectedType] = useState("")
     const [modalShow, setModalShow] = useState(false)
     const [{userId, equippedEquipment, stats, calcStats}, actions] = useGlobalState()
     const [Equipment, setEquipment] = useState([])
-    const [itemsByType, setItemsByType] = useState()
-    const [equipped, setEquipped] = useState()
+    const [selectedAbility, setSelectedAbility] = useState('frostarmor')
+    const [equippedAbilities, setEAs] = useState(["frostarmor", "healingtouch", "shiv", "shiv", "shiv"])
+    const [availableAbilities, setAAs] = useState(["frostarmor", "healingtouch", "shiv", "shiv", "shiv", "shiv", "shiv", "shiv", "shiv", "shiv", "shiv", "shiv"])
 
     useEffect(()=>{
 //        actions.getNewEquipment("basic_sword")
@@ -37,28 +39,78 @@ export default AbilitiesScreen = ({navigation}) => {
         setSelectedType(type)
         setEquipped(equippedEquipment[type])
     }
+
+    const abilityIconPress = (i) => {
+        setSelectedAbility(availableAbilities[i]);
+        console.log(selectedAbility);
+        setModalShow(true);
+    }
+
+    const renderAbility = ({ item, index }) => {
+        console.log(item);
+        return(
+            <TouchableOpacity style={{borderColor: "black", borderWidth: 1, borderRadius: 5, alignItems: "center", width:'80%', marginLeft:"10%", backgroundColor:'white'}}
+            onPress={()=>abilityIconPress(index)}>
+                <View style={{flexDirection:"row"}}>
+                    <Image source={SpellIcons[item]} onError={(e)=>console.log("err", e)}/>
+                    <View>
+                        <Text>{SpellInfo[item]['title']}</Text>
+                        <Text>{SpellInfo[item]['description']}</Text>
+                    </View>
+                </View>
+                
+            </TouchableOpacity>
+        )       
+    }
+
     return(
         <View style={styles.container}>
-            <Modal visible={modalShow} presentationStyle="overFullScreen" onDismiss={()=>setModalShow(false)}>
-                {itemsByType && <ItemFinder items={itemsByType} close={()=>setModalShow(false)} equipped={equipped} setEq={setEquipped}/> }
+            <Modal style={styles.abilityModal} visible={modalShow} presentationStyle="pageSheet">
+                <Image source = {SpellIcons[selectedAbility]}/>
+                <Text>{SpellInfo[selectedAbility]['title']}</Text>
+                <Text>{SpellInfo[selectedAbility]['description']}</Text>
+                <Button onPress={()=>{setModalShow(false)}} title="Go Back"/>
             </Modal>
             <View style={styles.equippedSpellsContainer}>
-                <Image source = {SpellIcons["frostarmor"]} style={styles.spellIconImage}/>
-                <Image source = {SpellIcons["healingtouch"]} style={styles.spellIconImage}/>
-                <Image source = {SpellIcons["shiv"]} style={styles.spellIconImage}/>
+                <TouchableOpacity onPress={()=>abilityIconPress(0)} style={styles.spellIconImage}>
+                <Image source = {SpellIcons["frostarmor"]}/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>abilityIconPress(1)} style={styles.spellIconImage}>
+                <Image source = {SpellIcons["healingtouch"]}/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>abilityIconPress(2)} style={styles.spellIconImage}>
+                <Image source = {SpellIcons["shiv"]}/>
+                </TouchableOpacity>
             </View>
             <View style={[styles.equippedSpellsContainer, {backgroundColor: 'red'}]}>
                 <Image source = {SpellIcons["shiv"]} style={styles.spellIconImage}/>
                 <Image source = {SpellIcons["shiv"]} style={styles.spellIconImage}/>
             </View>
             <View style={styles.availableSpellsContainer}> 
-                <Text>{SpellInfo["frostarmor"]['description']}</Text>
+            <FlatList
+                data={availableAbilities}
+                renderItem={renderAbility}
+                keyExtractor={(abilityName, index) => String(index)}
+                style={{flex: 1, width: "100%"}}
+                numColumns={1}
+            />
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    abilityModal: {
+        width: Dimensions.get('window').width * 0.8,
+        height: Dimensions.get('window').height * 0.5,
+        backgroundColor: 'rgba(30, 30, 40, 0.3)'
+    },
+    spellIconImage: {
+        height: "70%",
+        margin: "15%",
+        borderRadius: 10,
+        aspectRatio:1/1
+    },
     container: {
         flex: 1,
         width: "100%",
